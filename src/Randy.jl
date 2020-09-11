3b2655de2149a874a4a57a9fc4e026c563563606
Andrey Zakharevich <andreyzakharevich@gmail.com>
2020-09-11 10:31:12 +0300

    main function stub

--- src/Randy.jl
+++ src/Randy.jl
@@ -1,6 +1,33 @@
 module Randy
+using ArgParse
+import JSON
 
-greet() = print("Hello World!")
-greet()
+function generate_solution(train_data, fname, debug)
+end
+
+function validate_solution(solution, taskdata)
+    
+end
+
+function main()
+    s = ArgParseSettings()
+    @add_arg_table s begin
+        "--filename"
+            help = "task file to solve"
+            # arg_type = string
+            default = "data/training/ff28f65a.json"
+        "--debug"
+            action = :store_true
+    end
+    parsed_args = parse_args(ARGS, s)
+    taskdata = JSON.parsefile(parsed_args["filename"])
+
+    solution = generate_solution(taskdata["train"], split(split(parsed_args["filename"], '/')[end], '.')[1], parsed_args["debug"])
+    println(solution)
+    println(validate_solution(solution, taskdata["train"]))
+    println(validate_solution(solution, taskdata["test"]))
+end
+
+main()
 
 end # module
