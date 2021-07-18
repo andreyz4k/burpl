e1122987287ddbe96d18db9f1e32505ff4a11acf
Andrey Zakharevich <andreyzakharevich@gmail.com>
2021-07-18 18:12:50 +0300

    Output logs only for failed tasks

--- test/test_full_tasks.jl
+++ test/test_full_tasks.jl
@@ -1,3 +1,4 @@
+using Base: with_logger, SimpleLogger
 
 TASKS = [
     "../data/training/0a938d79.json",
@@ -48,52 +49,97 @@ using burpl: solve_and_check
 using Base.Iterators: flatten
 using Base.Threads: @spawn
 using GitHubActions: set_env
-using Test: Pass
+using Test: Pass, AbstractTestSet, Error, Fail, Broken, DefaultTestSet
+
+import Test: record, finish
+
+mutable struct LogFilterTestSet{T<:AbstractTestSet} <: AbstractTestSet
+    wrapped::T
+    description::String
+    log_file::String
+    stdout_file::String
+    LogFilterTestSet{T}(desc, log_file, stdout_file) where {T} = new(T(desc), desc, log_file, stdout_file)
+end
+LogFilterTestSet(desc; log_file = nothing, stdout_file = nothing, wrap = DefaultTestSet) =
+    LogFilterTestSet{wrap}(desc, log_file, stdout_file)
+
+
+record(ts::LogFilterTestSet, t) = record(ts.wrapped, t)
+
+function record(ts::LogFilterTestSet, t::Union{Fail,Error})
+    println("\n=====================================================")
+    printstyled(ts.description, "\n"; color = :white)
+    printstyled("Captured log output:\n"; color = :white)
+    for line in eachline(ts.log_file)
+        println(line)
+    end
+    printstyled("Captured stdout output:\n"; color = :white)
+    for line in eachline(ts.stdout_file)
+        println(line)
+    end
+    record(ts.wrapped, t)
+end
+
+
+function finish(ts::LogFilterTestSet)
+    finish(ts.wrapped)
+end
 
 function run_tasks(ch, tasks)
     asyncmap(tasks, ntasks = Threads.nthreads()) do fname
-        @time begin
-            fut = @spawn solve_and_check(fname)
-            timedwait(() -> istaskdone(fut), 300)
-            if !istaskdone(fut)
-                try
-                    schedule(fut, ErrorException("Timeout error"), error = true)
-                catch ex
-                    @warn(ex)
+        (log_file, log_io) = mktemp()
+        (stdout_file, stdout_io) = mktemp()
+        redirect_stdout(stdout_io) do
+            with_logger(SimpleLogger(log_io)) do
+                @time begin
+                    fut = @spawn solve_and_check(fname)
+                    timedwait(() -> istaskdone(fut), 300)
+                    if !istaskdone(fut)
+                        try
+                            schedule(fut, ErrorException("Timeout error"), error = true)
+                        catch ex
+                            @warn(ex)
+                        end
+                    end
                 end
+                flush(stdout_io)
+                flush(log_io)
+                put!(ch, (fname, fut, log_file, stdout_file))
             end
-            put!(ch, (fname, fut))
         end
+
     end
 end
 
 @testset "Full tasks" begin
     @info("Numthreads: $(Threads.nthreads())")
     taskref = Ref{Task}()
-    chnl = Channel(10, taskref=taskref) do ch
+    chnl = Channel(10, taskref = taskref) do ch
         run_tasks(ch, flatten([TASKS, UNSOLVED_TASKS]))
     end
 
     success_count = 0
 
     try
-        for _ in 1:length(TASKS) + length(UNSOLVED_TASKS)
-            (fname, fut) = take!(chnl)
-            if in(fname, TASKS)
-                @testset "run task $fname" begin
+        for _ = 1:length(TASKS)+length(UNSOLVED_TASKS)
+            (fname, fut, log_file, stdout_file) = take!(chnl)
+            @info(fname, log_file, stdout_file)
+            @testset LogFilterTestSet "run task $fname" log_file = log_file stdout_file = stdout_file wrap =
+                ExtendedTestSet begin
+                if in(fname, TASKS)
                     test_result = @test fetch(fut)
                     if isa(test_result, Pass)
                         success_count += 1
                     end
-                end
-            else
-                @testset "run task $fname" begin
+                else
                     @test !fetch(fut)
                 end
             end
         end
     catch ex
-        schedule(taskref, ex, error=true)
+        if !istaskdone(taskref[])
+            schedule(taskref[], ex, error = true)
+        end
         rethrow()
     end
 
